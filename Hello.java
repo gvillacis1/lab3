97e9f778b835a861b47986e7a5d84c64bdfaafde
AJE01 <114937040+AJE01@users.noreply.github.com>
2022-10-03 11:50:08 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     private static void main(String argv[]) {
         
-        System.out.println("Hello, " + argv[0] + "!");
+        System.out.println("Hello CSCI 4485" + argv[0] + "!");
     }
 }
